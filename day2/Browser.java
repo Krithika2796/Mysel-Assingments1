c3778cb4094feed68bcd2fe9d243282ff7c203f2
Krithika2796 <krithika2796@gmail.com>
2025-04-27 19:02:03 +0530

    Add files via upload
    Day 2

---
+++ day2/Browser.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Browser {
+	String launchbrowser(String browser) {
+		System.out.println(browser);
+		return "browsername";
+	}
+	public int LoadUrl() {
+	System.out.println("Application url loaded successfully");
+	return 3;
+	}
+	
+	}
+
+
